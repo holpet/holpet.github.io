7bcd9f558a0699f8b0997890987e995e8258b480
holpet <45787742+holpet@users.noreply.github.com>
2023-01-31 21:50:35 +0100

    Refactoring colors

--- client/src/components/Wrappers/ThemeWrapper.tsx
+++ client/src/components/Wrappers/ThemeWrapper.tsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
+
 interface props {
   children: React.ReactNode;
 }
 
 const ThemeWrapper = ({ children }: props) => {
-  return <div className="theme-wrapper theme-dark">{children}</div>;
+  const [colorTheme, setColorTheme] = useState("dark");
+
+  useEffect(() => {
+    const theme = localStorage.getItem("color-theme");
+    if (theme) {
+      setColorTheme(theme);
+    }
+    //document.documentElement.style.setProperty("--base-font-color", "red");
+  }, []);
+
+  return (
+    <div className={`theme-wrapper ${"theme-" + colorTheme}`}>{children}</div>
+  );
 };
 
 export default ThemeWrapper;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import ThemeWrapper from "./components/Wrappers/ThemeWrapper";
 import "./main.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+  <ThemeWrapper>
     <App />
-  </React.StrictMode>
+  </ThemeWrapper>
 );
