29ee4ed3f1dee291af7513dbbc47d48e78d92dfb
holpet <45787742+holpet@users.noreply.github.com>
2023-03-09 16:13:41 +0100

    updated demo #7

--- src/assets/demo/index.ts
+++ src/assets/demo/index.ts
@@ -1,4 +1,4 @@
 export { default as demo1 } from "./demo1.jpg";
 export { default as demo2 } from "./demo2.jpg";
-export { default as demo3 } from "./demob.jpg";
+export { default as demo3 } from "./demo1.jpg";
 export { default as demo4 } from "./demo4.jpg";
